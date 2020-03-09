2a29a80286d3d39bdea757fef7fc4f94a858ff15
Finn <finnterdal@gmail.com>
2020-03-09 09:58:20 -0500

    finish 2003-demo-1

---
+++ appetizers.js
@@ -0,0 +1,14 @@
+const tastyAppetizers = [
+  "jalapeño poppers",
+  "grilled calamari",
+  "mozz sticks",
+  "chicken liver paté"
+];
+
+const formatAppetizers = (apps) => {
+  return apps.join(", ")
+}
+
+module.exports = {
+  tastyAppetizers,
+  formatAppetizers
---
+++ menu.js
@@ -0,0 +1,4 @@
+const { tastyAppetizers, formatAppetizers } = require("./appetizers")
+
+console.log("Welcome to Basil Cafe! Here's our menu:")
+console.log("Appetizers: ", formatAppetizers(tastyAppetizers))
